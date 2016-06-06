b25968dc9cb5720fb7d016ca62c36b447bf9e7bc
PeterTong <tkwpeter21@gmail.com>
2016-06-06 13:56:45 +0800

    Initial Commit

---
+++ devslopes-firebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devslopes-firebase
+//
+//  Created by KwokWing Tong on 6/6/2016.
+//  Copyright © 2016 Tong Kwok Wing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
