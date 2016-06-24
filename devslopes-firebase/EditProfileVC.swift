fc1b6124d2325edc52aadd0b19f88e454bf8a02e
PeterTong <tkwpeter21@gmail.com>
2016-06-24 12:28:38 +0800

    use EZLoadingActivity for activity indicator view and set size classes for all iPhone landscape

--- devslopes-firebase/EditProfileVC.swift
+++ devslopes-firebase/EditProfileVC.swift
@@ -9,12 +9,13 @@
 import UIKit
 import Firebase
 import Alamofire
+import EZLoadingActivity
 
 class EditProfileVC: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate  {
 
   @IBOutlet weak var editProfileImage: UIImageView!
   @IBOutlet weak var editUsernameTxtField: MaterialTextField!
-  
+  @IBOutlet weak var UpdateProfileProgressView: UIProgressView!
   
  
   
@@ -27,6 +28,11 @@ class EditProfileVC: UIViewController,UIImagePickerControllerDelegate,UINavigati
   var usernames: String?
   var profileImgUrl: String?
   
+  var percent: Float?
+  
+
+  
+  
     override func viewDidLoad() {
         super.viewDidLoad()
       
@@ -34,9 +40,22 @@ class EditProfileVC: UIViewController,UIImagePickerControllerDelegate,UINavigati
         imagePicker.delegate = self
         // Do any additional setup after loading the view.
       editProfileImage.userInteractionEnabled = true
-     
+//     editProfileImage.layer.cornerRadius = editProfileImage.frame.size.width / 2
+//      editProfileImage.clipsToBounds = true
+      UpdateProfileProgressView.progress = 0
+      UpdateProfileProgressView.hidden = true
+      
+      EZLoadingActivity.Settings.FontName = "NotoSans-Regular"
+      EZLoadingActivity.Settings.ActivityColor = UIColor(red: 219/255, green: 223/255, blue: 34/255, alpha: 1.0)
+      EZLoadingActivity.Settings.SuccessText = "Success Update"
+      EZLoadingActivity.Settings.TextColor = UIColor(red: 219/255, green: 223/255, blue: 34/255, alpha: 1.0)
+      EZLoadingActivity.Settings.BackgroundColor = UIColor(red: 150/255, green: 102/255, blue: 50/255, alpha: 0.7)
+      EZLoadingActivity.Settings.SuccessColor = UIColor(red: 219/255, green: 223/255, blue: 34/255, alpha: 1.0)
+      EZLoadingActivity.hide()
       
     }
+  
+  
   
   override func viewWillAppear(animated: Bool) {
     super.viewWillAppear(animated)
@@ -44,11 +63,21 @@ class EditProfileVC: UIViewController,UIImagePickerControllerDelegate,UINavigati
     usernames = NSUserDefaults.standardUserDefaults().objectForKey(USERNAME_DEFAULT_KEY) as? String
     profileImgUrl = NSUserDefaults.standardUserDefaults().objectForKey(PROFILEIMAGE_DEFAULT_KEY) as? String
     var imageCache: UIImage?
-    if let url = profileImgUrl {
-      imageCache = FeedVC.imageCache.objectForKey(url) as? UIImage
+    if profileImgUrl != nil {
+      if let url = profileImgUrl {
+        imageCache = FeedVC.imageCache.objectForKey(url) as? UIImage
+      }
+      self.editProfileImage.hidden = true
+      
+      configureProfileView(usernames, profileURL: profileImgUrl, img: imageCache)
+    }else{
+      configureProfileView(usernames, profileURL: nil, img: editProfileImage.image)
     }
     
-    configureProfileView(usernames, profileURL: profileImgUrl, img: imageCache)
+    
+    
+    
+    
     
   }
   
@@ -57,9 +86,10 @@ class EditProfileVC: UIViewController,UIImagePickerControllerDelegate,UINavigati
   }
   
   func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
-    
+    NSUserDefaults.standardUserDefaults().removeObjectForKey(PROFILEIMAGE_DEFAULT_KEY)
     imagePicker.dismissViewControllerAnimated(true, completion: nil)
     editProfileImage.image = image
+    
     profileImageSelected = true
   }
 
@@ -67,7 +97,7 @@ class EditProfileVC: UIViewController,UIImagePickerControllerDelegate,UINavigati
     
     if let editTxt = editUsernameTxtField.text where editTxt != "" {
       
-      if let editImage = editProfileImage.image where profileImageSelected == true {
+      if let editImage = editProfileImage.image where profileImageSelected == true || profileImgUrl != ""{
         
 //        let urlStr = "https://post.imageshack.us/upload_api.php"
 //        let url = NSURL(string: urlStr)!
@@ -90,9 +120,28 @@ class EditProfileVC: UIViewController,UIImagePickerControllerDelegate,UINavigati
           
           switch encodeingResult{
           case .Success(request: let upload, streamingFromDisk: _, streamFileURL: _):
+            upload.progress({ (bytesWritten, totalBytesWritten, totalBytesExpectedToWrite) in
+              
+              dispatch_async(dispatch_get_main_queue()){
+                self.UpdateProfileProgressView.hidden = false
+                 self.percent = (Float(totalBytesWritten) / Float(totalBytesExpectedToWrite))
+                self.UpdateProfileProgressView.setProgress(self.percent!, animated: true)
+                print(self.percent!)
+                EZLoadingActivity.show("Updating your profile", disableUI: true)
+                
+                if self.percent! == 1.0 {
+                  self.UpdateProfileProgressView.hidden = true
+                  EZLoadingActivity.hide(success: true, animated: true)
+                }
+              }
+              
+              
+            })
+            
             upload.responseJSON(completionHandler: { (response) in
               
               let result = response.result
+              
               if let info = result.value as? Dictionary<String,AnyObject> {
                 if let links = info["links"] as? Dictionary<String, AnyObject> {
                   if let imageLink = links["image_link"] as? String {
@@ -105,6 +154,7 @@ class EditProfileVC: UIViewController,UIImagePickerControllerDelegate,UINavigati
             })
           case .Failure(let error):
             print(error)
+          
           }
           
         }
@@ -140,6 +190,8 @@ class EditProfileVC: UIViewController,UIImagePickerControllerDelegate,UINavigati
     
     profileImageSelected = false
     
+   
+    
     
   }
   
@@ -157,8 +209,10 @@ class EditProfileVC: UIViewController,UIImagePickerControllerDelegate,UINavigati
           
           if error == nil {
             
+            
             let image = UIImage(data: data!)!
             self.editProfileImage.image = image
+            self.editProfileImage.hidden = false
             FeedVC.imageCache.setObject(image, forKey: profileURL!)
           }
         })
